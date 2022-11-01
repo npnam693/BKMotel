76e65c174ec708ab8393d6ce2615cd642bdc1673
TruongNguyen1305 <trngcr71305@gmail.com>
2022-11-01 09:33:50 +0700

    add room api

--- server/routes/rooms.js
+++ server/routes/rooms.js
@@ -1,11 +1,11 @@
 import express from 'express';
+import { verifyToken } from '../middlewares/authMiddleware.js';
 import * as RoomController from '../controllers/RoomController.js'
 
 const router = express.Router();
 
-router.get('/', RoomController.RoomMenu)
-router.get('/getRooms', RoomController.getRooms )
-// router.post('/store', courseController.store);
-
+router.get('/', RoomController.roomMenu)
+router.get('/:id', RoomController.getRoom)
+router.get('/find', verifyToken, RoomController.findRooms)
 
 export default router;
