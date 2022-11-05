79b76f96ffd89034d74307ccddbe0f00ac1f3ce1
VanLam <vanlam.cs76@gmail.com>
2022-11-05 16:05:28 +0700

    create new branch

--- client/src/routes/index.js
+++ client/src/routes/index.js
@@ -1,12 +1,13 @@
 import DefaultLayout from "../layouts/DefaultLayout";
 
-import {HomePage, ProfilePage, DetailPage, FavouritePage} from '../pages'
+import {HomePage, ProfilePage, DetailPage, FavouritePage, PostPage} from '../pages'
 
 const publicRoutes = [
     { path: '/', component: HomePage, layout: DefaultLayout },
     { path: '/profile/:id', component: ProfilePage, layout: DefaultLayout },
     { path: '/detail/:id', component: DetailPage, layout: DefaultLayout },
     { path: '/favourite/:id', component: FavouritePage, layout: DefaultLayout },
+    { path: '/post', component: PostPage, layout: DefaultLayout}
 ];
 
 const privateRoutes = [];
