3d09dd19b32201b5c105e45bd18ef9bdbd72906b
TruongNguyen1305 <trngcr71305@gmail.com>
2022-11-25 23:25:47 +0700

    add modal clear

--- client/src/pages/Favourite/index.js
+++ client/src/pages/Favourite/index.js
@@ -1,10 +1,10 @@
 import styles from './style.module.css'
 import FavouriteItem from '../../components/FavouriteItem';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-import { Button, CircularProgress, Divider } from '@mui/material'
+import { Button, CircularProgress, Divider, Modal} from '@mui/material'
 import { useSnackbar } from 'notistack';
 import { UserState } from '../../Context/UserProvider'
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import axios from 'axios';
 
 
@@ -14,7 +14,7 @@ const theme = createTheme({
         MuiButton: {
             styleOverrides: {
                 root: {
-                    borderRadius: '20px',
+                    borderRadius: '15px',
                     fontSize: '12px',
                     fontFamily: '"Inter", sans-serif',
                     fontWeight: 600,
@@ -37,6 +37,9 @@ const theme = createTheme({
         bkmotel: {
             main: '#00A699',
         },
+        defaultBtn: {
+            main: '#ccc'
+        }
     },
 });
 
@@ -46,6 +49,7 @@ function FavouritePage() {
     const [favouriteRooms, setFavoriteRooms] = useState([])
     const [loading, setLoading] = useState(false)
     const { enqueueSnackbar, closeSnackbar } = useSnackbar();
+    const [modalOpen, setModalOpen] = useState(false)
     const toast = (message, variantType) => {
         enqueueSnackbar(message, {
             variant: variantType,
@@ -73,10 +77,24 @@ function FavouritePage() {
             })
     }
 
+    const handleRemove = useCallback((id)=>{
+        axios.put('/api/rooms/favourites/add', {
+            roomId: id
+        }, config)
+            .then(response=>{
+                setFavoriteRooms(response.data.favourites)
+                toast(response.data.message, 'success')
+            })
+            .catch(err=>{
+                toast(err.response.data.message, 'error')
+            })
+    }, [])
+
     useEffect(()=>{
         setLoading(true)
         axios.get(`/api/rooms/favourites/${userInfo._id}`, config)
             .then(res => {
+                // console.log(res)
                 setFavoriteRooms(res.data)
                 setLoading(false)
             })
@@ -94,15 +112,53 @@ function FavouritePage() {
                         variant="contained"
                         size='large'
                         color='bkmotel'
-                        onClick={()=>handleClear()}
+                        onClick={() =>{
+                                if (favouriteRooms.length)
+                                    setModalOpen(true)
+                            }
+                        }
                     >
                         Xóa tất cả
                     </Button>
+                    <Modal
+                        open={modalOpen}
+                        onClose={()=>setModalOpen(false)}
+                        aria-labelledby="modal-modal-title"
+                        aria-describedby="modal-modal-description"
+                    >
+                        <div className={styles.modal}>
+                            <header id="modal-modal-title"  className={styles.modalHeader}>
+                                Xác nhận xóa
+                            </header>
+                            <p id='modal-modal-description' className={styles.modalMessage}>Bạn có chắc chắn muốn xóa ?</p>
+                            <div className={styles.modalFooter}>
+                                <Button
+                                    variant="contained"
+                                    size='small'
+                                    color='bkmotel'
+                                    onClick={() => {
+                                        handleClear()
+                                        setModalOpen(false)
+                                    }}
+                                >
+                                    Xác nhận
+                                </Button>
+                                <Button
+                                    variant="contained"
+                                    size='small'
+                                    color='info'
+                                    onClick={() => setModalOpen(false)}
+                                >
+                                    Hủy xóa
+                                </Button>
+                            </div>
+                        </div>
+                    </Modal>
                 </div>
                 <Divider variant="middle"  />
                 {loading ? (<CircularProgress size={100}/>) : (
                     <div className={styles.listItem}>
-                        {!favouriteRooms.length ? (<span style={{fontSize: '20px'}}>Danh sách trống</span>) : (favouriteRooms.map(room => <FavouriteItem key={room._id} data ={room}/>))}
+                        {!favouriteRooms.length ? (<span style={{fontSize: '20px'}}>Danh sách trống</span>) : (favouriteRooms.map(room => <FavouriteItem key={room._id} data ={room} onRemove={handleRemove}/>))}
                     </div>
                 )}
             </div> 
