31573fe6b074c50b894f88c9ca192c474bbaa37e
vinhkim1409 <80050330+vinhkim1409@users.noreply.github.com>
2022-11-30 20:57:33 +0700

    Update ReviewController.js
    Signed-off-by: vinhkim1409 <80050330+vinhkim1409@users.noreply.github.com>

--- server/controllers/ReviewController.js
+++ server/controllers/ReviewController.js
@@ -1,3 +1,93 @@
-import Review from "../models/Review";
 
-//comments
+import Room from '../models/Room.js'
+import Review from '../models/Review.js'
+import User from '../models/User.js'
+
+
+// @create review 
+// [POST] /api/reviews/add
+// push review to array of Room and to array of User
+export const AddReview = async (req,res) =>{
+    const {ratingPoint,description} = req.body
+    const { roomId } = req.params.id
+    const userId = req.userId
+    
+   try {
+    const newReview = new Review({
+        ratingPoint,
+        description,
+       creator:userId,
+        room:roomId
+    }) 
+    await newReview.save()
+    const reviewId=newReview.id
+    let query 
+    query=  {
+        $push: { reviews: reviewId }
+    }
+    User.findByIdAndUpdate(userId,
+        query,
+        {new: true}
+        ).populate('reviews')
+    
+    res.json({ success: true, review: newReview })
+   } catch (error) {
+    console.log(error)
+		res.status(500).json({ success: false, message: 'Internal server error' })
+   }
+   
+    
+}
+//get review
+// [GET] /api/reviews/reviewuser
+export const GetReviewUser = async (req,res)=>{
+    const userId = req.user._id
+    try {
+        const reviews= await Review.find({creator:userId}).populate('creator',['name'])
+        res.json({success:true, reviews})
+    } catch (error) {
+        console.log(error)
+		res.status(500).json({ success: false, message: 'Internal server error' })
+    }
+
+}
+//[GET] /api/reviews/reviewroom
+export const GetReviewRoom =async (req,res)=>{
+    const roomId = req.params.id
+    try {
+        //sort -password
+        const reviews =await Review.find({room:roomId}).populate('creator',['name'])
+        res.json({success:true, reviews})
+    } catch (error) {
+        console.log(error)
+		res.status(500).json({ success: false, message: 'Internal server error' })
+    }
+}
+//put UpdateReview
+// [PUT] [GET] /api/reviews/updatereview/:id
+export const UpdateReview =async (req,res)=>{
+    const {ratingPoint,description} = req.body
+    const userId = req.user._id
+    try {
+        let updateReview = {
+            ratingPoint,
+            description: description ||'',
+            creator:userId
+           
+        }
+        const ReviewUpdateCondition ={_id:req.params.id,creator:userId}
+        updateReview = await Review.findOneAndUpdate(ReviewUpdateCondition,updateReview,{new:true})
+        if(!updateReview)
+        {
+            return res.status(401).json({success: false, message: 'Review not found or user not authorised'})
+        }
+        res.json({success:true, message:'Update success',review:updateReview})
+    } catch (error) {
+        console.log(error)
+		res.status(500).json({ success: false, message: 'Internal server error' })
+    }
+
+}
+export const DeleteReview = (req,res) => {
+    res.send('delete Review')
+}
